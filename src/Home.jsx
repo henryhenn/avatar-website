528542673d8f232db132ac4947e1c726ca0ce850
= <=>
2023-05-12 18:23:14 +0700

    Installed react-router-dom, TailwindCSS, configured react-router-dom, created pages for web

---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import Main from "./layouts/Main";
+
+export default function Home() {
+    return (
+        <Main>
+            <div>
+                <h1 className="text-2xl font-bold">Tes</h1>
+            </div>
+        </Main>
+    )
+}
